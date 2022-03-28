a7c55f0517336e3bb72b73d81931265666bed015
Robin Message <robin@lambdacambridge.com>
2022-03-28 17:56:06 +0100

    Initial value editor

--- src/components/semantic/SemanticItem.tsx
+++ src/components/semantic/SemanticItem.tsx
@@ -1,15 +1,16 @@
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent, useRef, useState } from "react";
 
 import styles from "./styles.module.css";
 
-import { ValuePresenter } from "./ValuePresenter";
+import { ValuePresenter, ValuePresenterRef } from "./ValuePresenter";
 import { Content } from "../../isaac-data-types";
 import { ListChildrenPresenter } from "./ListChildrenPresenter";
 import { AccordionPresenter } from "./AccordionPresenter";
 import {
     AnswerPresenter,
-    MultipleChoiceQuestionPresenter, NumericQuestionPresenter, QuestionBodyPresenter,
-    QuestionMetaPresenter,
+    MultipleChoiceQuestionPresenter,
+    NumericQuestionPresenter,
+    QuestionBodyPresenter,
     QuickQuestionPresenter, SymbolicQuestionPresenter
 } from "./QuestionPresenters";
 import { TabsPresenter } from "./TabsPresenter";
@@ -47,7 +48,7 @@ interface RegistryEntry {
     name?: string;
     metadataPresenter?: Presenter;
     childrenPresenter?: Presenter;
-    valuePresenter?: Presenter;
+    valuePresenter?: typeof ValuePresenter;
     additionalPresenter?: Presenter;
 }
 
@@ -65,7 +66,7 @@ const choicesEntry: RegistryEntry = {
 
 const choiceEntry: RegistryEntry = {
     name: "Choice",
-    additionalPresenter: ChoicePresenter,
+    additionalPresenter: ChoicePresenter, // FIXME: this should be a ValuePresenter
 };
 
 const pageEntry: RegistryEntry = {
@@ -134,12 +135,15 @@ interface BoxProps {
     name?: string | undefined;
     onDelete?: () => void;
     className?: string;
+    onClick?: () => void;
 }
 
-export const Box: FunctionComponent<BoxProps> = ({name, onDelete, className, children}) => {
+export const Box: FunctionComponent<BoxProps> = ({name, onDelete, className, onClick, children}) => {
     const [deleteHovered, setDeleteHovered] = useState(false);
     return <div className={`${styles.box} ${className ?? ""} ${deleteHovered ? styles.boxDeleteHovered : ""}`}>
-        <div className={styles.boxHeader}>
+        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+        <div className={styles.boxHeader}
+             onClick={onClick}>
             {name && <span className={styles.boxLabel}>{name}</span>}
             <span className={styles.boxSpacer}/>
             {onDelete && <button className={styles.boxDelete}
@@ -155,6 +159,7 @@ export const Box: FunctionComponent<BoxProps> = ({name, onDelete, className, chi
 };
 
 export function SemanticItem({doc, update, onDelete, name, className}: SemanticItemProps) {
+    const valueRef = useRef<ValuePresenterRef>(null);
     const typeWithLayout = `${doc.type}$${doc.layout}` as TYPES;
     const entryType = REGISTRY[typeWithLayout] || REGISTRY[doc.type as TYPES] || REGISTRY.content;
 
@@ -165,13 +170,13 @@ export function SemanticItem({doc, update, onDelete, name, className}: SemanticI
     const children = doc.children && ChildrenPresenter ? <ChildrenPresenter doc={doc} update={update} /> : null;
 
     const ValuePresenter = entryType.valuePresenter;
-    const value = doc.value && ValuePresenter ? <ValuePresenter doc={doc} update={update} /> : null;
+    const value = doc.value && ValuePresenter ? <ValuePresenter doc={doc} update={update} ref={valueRef} /> : null;
 
     const AdditionalPresenter = entryType.additionalPresenter;
     const additional = AdditionalPresenter ? <AdditionalPresenter doc={doc} update={update} /> : null;
 
     // Render outline with type name
-    return <Box name={name || entryType.name} onDelete={onDelete} className={className}>
+    return <Box name={name || entryType.name} onDelete={onDelete} className={className} onClick={() => valueRef.current?.startEdit()}>
         {metadata}
         {value}
         {children}
