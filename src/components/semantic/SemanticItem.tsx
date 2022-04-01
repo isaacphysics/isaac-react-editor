e4851f8e50e809060334b7337233227411ea153a
Robin Message <robin@lambdacambridge.com>
2022-04-01 11:20:51 +0100

    JSONEditor for any block

--- src/components/semantic/SemanticItem.tsx
+++ src/components/semantic/SemanticItem.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, useRef, useState } from "react";
 import CodeMirror, { EditorView } from '@uiw/react-codemirror';
-import { json } from "@codemirror/lang-json";
+import { linter } from '@codemirror/lint';
+import { json, jsonParseLinter } from '@codemirror/lang-json';
 
 import styles from "./styles.module.css";
 
@@ -25,17 +26,18 @@ export interface SemanticItemProps {
 
 interface BoxProps {
     name?: string | undefined;
+    onClick?: () => void;
     onDelete?: () => void;
     className?: string;
     valueRef?: ValueRef;
     shift?: Shift;
 }
 
-export const Box: FunctionComponent<BoxProps> = ({name, onDelete, shift, className, valueRef, children}) => {
+export const Box: FunctionComponent<BoxProps> = ({name, onClick,  onDelete, shift, className, valueRef, children}) => {
     const [deleteHovered, setDeleteHovered] = useState(false);
     return <ValueWrapper className={`${styles.box} ${className ?? ""} ${deleteHovered ? styles.boxDeleteHovered : ""}`} valueRef={valueRef}>
         <div className={styles.boxHeader}>
-            {name && <span className={styles.boxLabel}>{name}</span>}
+            {name && <button className={styles.boxLabel} onClick={onClick} disabled={!onClick}>{name}</button>}
             <span className={styles.boxSpacer}/>
             {shift && shift.up && <button className={styles.boxUp}
                                           onClick={() => shift?.by(-1)}>
@@ -60,20 +62,24 @@ export const Box: FunctionComponent<BoxProps> = ({name, onDelete, shift, classNa
 
 function SemanticItemInner(props: SemanticItemProps) {
     const {doc, update, name, ...rest} = props;
+    const subProps = {doc, update};
     const valueRef = useRef<ValuePresenterRef>(null);
+
+    const [jsonMode, setJsonMode] = useState(false);
+
     const entryType = getEntryType(doc);
 
     const MetadataPresenter = entryType.headerPresenter;
-    const metadata = MetadataPresenter ? <MetadataPresenter doc={doc} update={update} /> : null;
+    const metadata = !jsonMode && MetadataPresenter ? <MetadataPresenter {...subProps} /> : null;
 
     const BodyPresenter = entryType.bodyPresenter;
-    const body = BodyPresenter ? <BodyPresenter doc={doc} update={update} valueRef={valueRef} /> : null;
+    const body = jsonMode ? <JSONEditor {...subProps} close={() => setJsonMode(false)} /> : BodyPresenter ? <BodyPresenter {...subProps} valueRef={valueRef} /> : null;
 
     const AdditionalPresenter = entryType.footerPresenter;
-    const additional = AdditionalPresenter ? <AdditionalPresenter doc={doc} update={update} /> : null;
+    const additional = !jsonMode && AdditionalPresenter ? <AdditionalPresenter {...subProps} /> : null;
 
     // Render outline with type name
-    return <Box name={name || entryType.name} {...rest} valueRef={valueRef}>
+    return <Box name={name || entryType.name} {...rest} valueRef={valueRef} onClick={() => setJsonMode(true)}>
         {metadata}
         {body}
         {additional}
@@ -82,26 +88,39 @@ function SemanticItemInner(props: SemanticItemProps) {
 
 const SemanticItemInnerMemo = React.memo(SemanticItemInner);
 
+const extensions = [json(), EditorView.lineWrapping, linter(jsonParseLinter())];
+
 const empty = Symbol("empty") as unknown as string;
-function JSONEditor({doc, update}: PresenterProps) {
+function JSONEditor({doc, update, close}: PresenterProps & {close: () => void}) {
     const value = useRef(empty);
     if (value.current === empty) {
         value.current = JSON.stringify(doc, null, 2);
     }
+    const [valid, setValid] = useState(true);
     return <>
         <CodeMirror
             value={value.current}
-            extensions={[json(), EditorView.lineWrapping]}
+            maxHeight="calc(100vh - 120px)"
+            extensions={extensions}
             onChange={(newValue) => {
                 value.current = newValue;
+                try {
+                    JSON.parse(newValue);
+                    setValid(true);
+                } catch (e) {
+                    console.error(e);
+                    setValid(false);
+                }
             }}
         />
         <div className={styles.editButtons}>
-            <Button onClick={(e) => {
+            <Button onClick={() => {
                 value.current = JSON.stringify(doc, null, 2);
+                close();
             }}>Cancel</Button>
-            <Button color="primary" onClick={(e) => {
+            <Button color="primary" disabled={!valid} onClick={() => {
                 update(JSON.parse(value.current));
+                close();
             }}>Save</Button>
         </div>
     </>;
@@ -129,8 +148,9 @@ export class SemanticItem extends React.Component<SemanticItemProps, { hasError:
                 </Alert>
                 <JSONEditor {...this.props} update={(newContent) => {
                     this.props.update(newContent);
+                }} close={() => {
                     this.setState({hasError: false});
-                }} />
+                }}/>
             </Box>;
         }
         return <SemanticItemInnerMemo {...this.props} />;
