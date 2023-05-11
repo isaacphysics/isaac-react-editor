8712049b8e37c50a2a9057e5ea694f04d024c385
Chris Purdy <33040507+chrisjpurdy@users.noreply.github.com>
2023-05-11 12:10:03 +0100

    Change to correct Ada content repo in localhost config

--- src/services/config.ts
+++ src/services/config.ts
@@ -22,7 +22,7 @@ const configs: { [host: string]: Config } = {
         ...sharedConfig,
         clientId: "f62f2bd4954bf930bc3f",
         authCode: "/vFaDdbb7id6+cPgsIKTdvwk4lOLBkBpBsXDdBvZsnU0U/PBLxgzxDzmUfE/0OIWWvlxh7SigvVv1JzBffbEc2364W8GPmQt9QeuVW1juAHdvdT7kRrHv8LjEuxJP9ie9+BP3tXNWpVxdg7S3sbZA5ShBFOYxdr3izjn9L+cmzDT9YVKB+Grv8hvLcEFOy7KHeixa29HPY2pqtk6XHFqiwlDP+02AWmY",
-        REPO: siteSpecific("rutherford-content", "isaac-content-2"),
+        REPO: siteSpecific("rutherford-content", "ada-content"),
         previewServer: siteSpecific("http://localhost:3002", "http://localhost:3001"),
     },
     "editor.isaacphysics.org": {
