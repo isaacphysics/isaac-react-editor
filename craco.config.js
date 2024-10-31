f35713756bf8d6b2341d65f4fc2dbc685a48f171
Meurig Thomas <mlt47@cam.ac.uk>
2024-10-31 01:01:33 +0000

    Use CRACO to configure CRA without ejecting

---
+++ craco.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    webpack: {
+      configure: (webpackConfig, { env, paths }) => {
+        console.log("TODO MT - Add a loader for .nearley files");
+        return webpackConfig;
+      },
+    },
+  };
