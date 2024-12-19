2741b6353d2557cab7138160c035d4cf40d2dfcc
Alex Lewin <sl2281@cam.ac.uk>
2024-12-19 13:53:00 +0000

    Restore ordered property

--- src/components/semantic/presenters/questionPresenters.tsx
+++ src/components/semantic/presenters/questionPresenters.tsx
@@ -152,6 +152,7 @@ export function changeQuestionType({doc, update, newType} : PresenterProps & {ne
     }
 
     if (newType !== "isaacCoordinateQuestion") {
+        delete newDoc.ordered;
         delete newDoc.numberOfCoordinates;
         delete newDoc.numberOfDimensions;
     }
